d0a311c977bdf29a4f0a0ad03b02143b15916bcd
Celio23 <52529824+Celio23@users.noreply.github.com>
2024-02-23 20:48:32 +0200

    Add files via upload

---
+++ Day15P1.cs
@@ -0,0 +1,22 @@
+﻿var input = File.ReadAllText("input.txt");
+
+var parts = input.Split(",", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+var result = 0;
+
+foreach(var part in parts)
+{
+    result += Hash(part);
+}
+
+Console.WriteLine(result);
+
+int Hash(string input)
+{
+    int currentValue = 0;
+        foreach (var ch in input)
+        {
+            currentValue = (currentValue + (int)ch) * 17 % 256;
+        }
+        return currentValue;
+}

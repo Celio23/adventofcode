539170036fca36ef6cd2176f923668de5deb7cc5
Celio23 <52529824+Celio23@users.noreply.github.com>
2024-02-25 01:36:19 +0200

    Add files via upload

---
+++ Day2/Day2P2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+
+var runningTotal = 0;
+
+string[] lines = File.ReadAllLines("input.txt");
+
+foreach (var line in lines)
+{
+    var gameInfo = line.Split(':');
+
+    if (gameInfo.Length != 2)
+    {
+        Console.WriteLine("Invalid input format. Skipping line.");
+        continue;
+    }
+
+    var gameId = int.Parse(gameInfo[0].Split(' ')[1]);
+    var rounds = gameInfo[1].Split(';', StringSplitOptions.TrimEntries);
+    var maxRed = 0;
+    var maxGreen = 0;
+    var maxBlue = 0;
+
+    foreach (var round in rounds)
+    {
+        var colorInfos = round.Split(',', StringSplitOptions.TrimEntries);
+
+        foreach (var color in colorInfos)
+        {
+            var colorInfo = color.Split(' ');
+
+            if (colorInfo.Length != 2)
+            {
+                Console.WriteLine("Invalid color format. Skipping round.");
+                continue;
+            }
+
+            var colorCount = int.Parse(colorInfo[0]);
+            var colorName = colorInfo[1].ToLower(); // Convert color name to lowercase for case-insensitive comparison
+
+            switch (colorName)
+            {
+                case "red":
+                    maxRed = Math.Max(colorCount, maxRed);
+                    break;
+                case "green":
+                    maxGreen = Math.Max(colorCount, maxGreen);
+                    break;
+                case "blue":
+                    maxBlue = Math.Max(colorCount, maxBlue);
+                    break;
+                default:
+                    Console.WriteLine($"Unknown color '{colorName}'. Skipping round.");
+                    break;
+            }
+        }
+    }
+
+    var product = maxRed * maxGreen * maxBlue;
+    runningTotal += product;
+}
+
+Console.WriteLine(runningTotal);
